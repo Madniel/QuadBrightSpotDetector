4801a7d85b8c3b430b5afa7a462755099be3346a
madniel <michalfeliks19@gmail.com>
2023-09-19 12:00:13 +0200

    refactoring

--- brightness_patch_detector/brightness_path_detector.py
+++ brightness_patch_detector/brightness_path_detector.py
@@ -1,25 +1,21 @@
 from pathlib import Path
-from typing import List, Any
+from typing import List
 
 import cv2
 import numpy as np
-from numpy import floating, ndarray
 
 KERNEL_SIZE = 5
 NUMBER_PATCHES = 4
-
+PATCH_CENTER_OFFSET = KERNEL_SIZE // 2
 
 def get_average_brightness(image: np.ndarray, x: int, y: int) -> float:
-    patch = image[y:y + KERNEL_SIZE, x:x + KERNEL_SIZE]
-    return np.mean(patch)
+    return np.mean(image[y:y + KERNEL_SIZE, x:x + KERNEL_SIZE]).astype(np.float32)
 
 
 def get_patches(image: np.ndarray, height: int, width: int) -> List:
-    patches = []
-    for y in range(height - KERNEL_SIZE):
-        for x in range(width - KERNEL_SIZE):
-            average_brightness = get_average_brightness(image, x, y)
-            patches.append(((x + 2, y + 2), average_brightness))
+    patches = [((x + PATCH_CENTER_OFFSET, y + PATCH_CENTER_OFFSET), get_average_brightness(image, x, y))
+               for x in range(width - KERNEL_SIZE)
+               for y in range(height - KERNEL_SIZE)]
     return patches
 
 
@@ -28,15 +24,15 @@ def get_sorted_patches(image: np.ndarray, height: int, width: int) -> List:
 
 
 def get_selected_patches(sorted_patches: List, grid: np.ndarray, height: int, width: int) -> np.ndarray:
-    selected_patches = np.empty((0, 2), int)  # Initialize an empty numpy array
+    selected_patches = np.empty((0, PATCH_CENTER_OFFSET), int)  # Initialize an empty numpy array
     for patch in sorted_patches:
         coord, _ = patch
         grid_x = coord[0] // KERNEL_SIZE
         grid_y = coord[1] // KERNEL_SIZE
         if not grid[grid_y, grid_x]:  # if the cell is not occupied
             selected_patches = np.vstack([selected_patches, np.array([coord])])  # Stack the new coordinate
-            grid[max(grid_y - 1, 0):min(grid_y + 2, height // KERNEL_SIZE),
-            max(grid_x - 1, 0):min(grid_x + 2, width // KERNEL_SIZE)] = True
+            grid[max(grid_y - 1, 0):min(grid_y + PATCH_CENTER_OFFSET, height // KERNEL_SIZE),
+            max(grid_x - 1, 0):min(grid_x + PATCH_CENTER_OFFSET, width // KERNEL_SIZE)] = True
             if selected_patches.shape[0] == NUMBER_PATCHES:
                 break
     return selected_patches
@@ -61,24 +57,28 @@ def get_top_patches(image: np.ndarray) -> np.ndarray:
                                 width=width, )
 
 
-def calculate_area_of_quadrilateral(x: List, y: List) -> float:
-    return 0.5 * abs(sum(x[i] * y[i + 1] - x[i + 1] * y[i] for i in range(3)) + x[3] * y[0] - x[0] * y[3])
+def get_shoelace_formula_result(x_coordinates: np.ndarray, y_coordinates: np.ndarray) -> float:
+    forward_diagonal_product = x_coordinates * np.roll(y_coordinates, -1)
+    backward_diagonal_product = y_coordinates * np.roll(x_coordinates, -1)
+
+    return 0.5 * abs(np.sum(forward_diagonal_product - backward_diagonal_product))
 
 
 def get_area_of_quadrilateral(points: np.ndarray) -> float:
-    x = [p[0] for p in points]
-    y = [p[1] for p in points]
-    return calculate_area_of_quadrilateral(x, y)
+    height_coordinates = points[:, 0]
+    width_coordinates = points[:, 1]
+
+    return get_shoelace_formula_result(height_coordinates, width_coordinates)
 
 
 def draw_and_save(image: np.ndarray, points: np.ndarray, output_path: str) -> None:
-    img_copy = image.copy()
-    img_colored = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
+    image_copy = image.copy()
+    image_colored = cv2.cvtColor(image_copy, cv2.COLOR_GRAY2BGR)
 
-    points = points.reshape((-1, 1, 2))
+    points = points.reshape((1, -1, 1, 2))
 
-    cv2.polylines(img_colored, [points], isClosed=True, color=(0, 0, 255), thickness=2)
-    cv2.imwrite(output_path, img_colored)
+    cv2.polylines(image_colored, points, isClosed=True, color=(0, 0, 255), thickness=2)
+    cv2.imwrite(output_path, image_colored)
 
 
 def process_image(image_path: Path, output_path: Path) -> None:
