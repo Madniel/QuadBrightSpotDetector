e7c906b7de6192f2ba7457df62082857e9ca6c9c
Michael <michalfeliks19@gmail.com>
2023-09-20 20:22:41 +0200

    modify requirements and setup.py to not be depended on version

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.21.2',
-        'opencv_python~=4.8.0.76',
-        'pytest~=7.1.1',
-        'setuptools~=61.2.0'
+        'opencv_python',
+        'pytest',
+        'setuptools'
     ],
 )
